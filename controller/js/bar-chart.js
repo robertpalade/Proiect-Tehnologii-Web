3e4a0d0415e83ae653d7d245d1c8d330e21dc123
robertpalade <61385985+robertpalade@users.noreply.github.com>
2020-05-11 11:13:35 +0300

    added download as CSV button to pie-chart.html

--- controller/js/bar-chart.js
+++ controller/js/bar-chart.js
@@ -1,6 +1,6 @@
-var county = document.getElementById('selectCountyButton').value.toUpperCase()
-var theUrl = "http://localhost/api/masina/count_cars.php?county="
-theUrl = theUrl.concat(county)
+var county = document.getElementById("selectCountyButton").value.toUpperCase();
+var theUrl = "http://localhost/api/masina/count_cars.php?county=";
+theUrl = theUrl.concat(county);
 console.log(theUrl);
 var req = new XMLHttpRequest();
 req.overrideMimeType("application/json");
@@ -13,7 +13,6 @@ req.onload = function () {
 req.send();
 
 function buildAll(data) {
-
     // set the dimensions and margins of the graph
     var margin = { top: 30, right: 30, bottom: 70, left: 60 },
         width = 460 - margin.left - margin.right,
@@ -23,18 +22,22 @@ function buildAll(data) {
     var svg = d3
         .select("#my_dataviz")
         .append("svg")
-        .attr('id', 'svg')
+        .attr("id", "svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    document.getElementById('selectCountyButton').addEventListener('change', updateBar)
+    document
+        .getElementById("selectCountyButton")
+        .addEventListener("change", updateBar);
 
     function updateBar() {
-        var county = document.getElementById('selectCountyButton').value.toUpperCase()
-        var theUrl = "http://localhost/api/masina/count_cars.php?county="
-        theUrl = theUrl.concat(county)
+        var county = document
+            .getElementById("selectCountyButton")
+            .value.toUpperCase();
+        var theUrl = "http://localhost/api/masina/count_cars.php?county=";
+        theUrl = theUrl.concat(county);
         console.log(theUrl);
         var req = new XMLHttpRequest();
         req.overrideMimeType("application/json");
@@ -46,31 +49,42 @@ function buildAll(data) {
             var x = d3
                 .scaleBand()
                 .range([0, width])
-                .domain(data.map(function (d) { return d.year; }))
+                .domain(
+                    data.map(function (d) {
+                        return d.year;
+                    })
+                )
                 .padding(0.2);
-            var y = d3.scaleLinear()
-                .domain([0, 320000])
-                .range([height, 0]);
+            var y = d3.scaleLinear().domain([0, 320000]).range([height, 0]);
 
             d3.selectAll(".bar")
                 .data(data1)
-                .transition().duration(1000)
-                .attr("x", function (d) { return x(d.year); })
-                .attr("y", function (d) { return y(d.number_of_cars); })
-                .attr("height", function (d) { return height - y(d.number_of_cars); });
-
+                .transition()
+                .duration(1000)
+                .attr("x", function (d) {
+                    return x(d.year);
+                })
+                .attr("y", function (d) {
+                    return y(d.number_of_cars);
+                })
+                .attr("height", function (d) {
+                    return height - y(d.number_of_cars);
+                });
         };
         req.send();
     }
 
     function buildBar(svg, data) {
-
         // X axis
         console.log(data);
         var x = d3
             .scaleBand()
             .range([0, width])
-            .domain(data.map(function (d) { return d.year; }))
+            .domain(
+                data.map(function (d) {
+                    return d.year;
+                })
+            )
             .padding(0.2);
 
         svg.append("g")
@@ -81,12 +95,9 @@ function buildAll(data) {
             .style("text-anchor", "end");
 
         // Add Y axis
-        var y = d3.scaleLinear()
-            .domain([0, 320000])
-            .range([height, 0]);
+        var y = d3.scaleLinear().domain([0, 320000]).range([height, 0]);
 
-        svg.append("g")
-            .call(d3.axisLeft(y));
+        svg.append("g").call(d3.axisLeft(y));
 
         // Bars
         svg.selectAll(".bar")
@@ -94,50 +105,64 @@ function buildAll(data) {
             .enter()
             .append("rect")
             .attr("class", "bar")
-            .attr("x", function (d) { return x(d.year); })
-            .attr("y", function (d) { return y(d.number_of_cars); })
+            .attr("x", function (d) {
+                return x(d.year);
+            })
+            .attr("y", function (d) {
+                return y(d.number_of_cars);
+            })
             .attr("width", x.bandwidth())
-            .attr("height", function (d) { return height - y(d.number_of_cars); })
-            .attr("fill", "#69b3a2")
+            .attr("height", function (d) {
+                return height - y(d.number_of_cars);
+            })
+            .attr("fill", "#69b3a2");
     }
 
-    buildBar(svg, data)
+    buildBar(svg, data);
+}
 
-    document.getElementById('downloadSVGButton').addEventListener('click', () => saveSvg(document.getElementById('svg'), 'image.svg'))
+document
+    .getElementById("downloadSVGButton")
+    .addEventListener("click", () =>
+        saveSvg(document.getElementById("svg"), "image.svg")
+    );
 
-    function saveSvg(svgEl, name) {
-        svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        var svgData = svgEl.outerHTML;
-        var preface = '<?xml version="1.0" standalone="no"?>\r\n';
-        var svgBlob = new Blob([preface, svgData], { type: "image/svg+xml;charset=utf-8" });
-        var svgUrl = URL.createObjectURL(svgBlob);
-        var downloadLink = document.createElement("a");
-        downloadLink.href = svgUrl;
-        downloadLink.download = name;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-    }
+function saveSvg(svgEl, name) {
+    svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    var svgData = svgEl.outerHTML;
+    var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+    var svgBlob = new Blob([preface, svgData], {
+        type: "image/svg+xml;charset=utf-8",
+    });
+    var svgUrl = URL.createObjectURL(svgBlob);
+    var downloadLink = document.createElement("a");
+    downloadLink.href = svgUrl;
+    downloadLink.download = name;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
 
-    document.getElementById('webpDownloadButton').addEventListener('click', () => saveWebp())
+document
+    .getElementById("webpDownloadButton")
+    .addEventListener("click", () => saveWebp());
 
-    function saveWebp() {
-        // the canvg call that takes the svg xml and converts it to a canvas
-        var svgData = document.getElementById("svg").outerHTML;
-        const canvas = document.createElement('canvas');
-        canvas.setAttribute('id', 'svg-canvas')
-        canvg(canvas, svgData);
+function saveWebp() {
+    // the canvg call that takes the svg xml and converts it to a canvas
+    var svgData = document.getElementById("svg").outerHTML;
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "svg-canvas");
+    canvg(canvas, svgData);
 
-        // the canvas calls to output a png
-        var img = canvas.toDataURL("image/webp");
-        // do what you want with the base64, write to screen, post to server, etc...
+    // the canvas calls to output a png
+    var img = canvas.toDataURL("image/webp");
+    // do what you want with the base64, write to screen, post to server, etc...
 
-        var downloadLink = document.createElement("a");
-        downloadLink.href = img;
-        downloadLink.download = name;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-        document.body.removeChild(canvas)
-    }
+    var downloadLink = document.createElement("a");
+    downloadLink.href = img;
+    downloadLink.download = name;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    document.body.removeChild(canvas);
 }
--- controller/js/pie-chart.js
+++ controller/js/pie-chart.js
@@ -1,66 +1,78 @@
-var county = document.getElementById('selectCountyButton').value.toUpperCase()
-var theUrl = "http://localhost/api/masina/count_cars.php?county="
-theUrl = theUrl.concat(county)
-console.log(theUrl);
+var county = document.getElementById("selectCountyButton").value.toUpperCase();
+var theUrl = "http://localhost/api/masina/count_cars.php?county=";
+var responseData;
+theUrl = theUrl.concat(county);
 var req = new XMLHttpRequest();
 req.overrideMimeType("application/json");
 req.open("GET", theUrl, true);
 req.onload = function () {
-    var jsonResponse = JSON.parse(req.responseText);
-    buildAll(jsonResponse.records);
+    responseData = JSON.parse(req.responseText);
+    console.log("responseData", responseData);
+    buildAll(responseData.records);
 };
 
 req.send();
+
+console.log("responseData", responseData);
+
 function buildAll(data) {
     // set the dimensions and margins of the graph
-    var width = 700
-    height = 450
-    height = 450
-    margin = 40
+    var width = 700;
+    height = 450;
+    height = 450;
+    margin = 40;
 
     // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-    var radius = Math.min(width, height) / 2 - margin
+    var radius = Math.min(width, height) / 2 - margin;
 
     // append the svg object to the div called 'my_dataviz'
     var svg = d3
-        .select('#my_dataviz')
-        .append('svg')
-        .attr('id', 'svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', 'translate(' + width / 3 + ',' + height / 2 + ')')
+        .select("#my_dataviz")
+        .append("svg")
+        .attr("id", "svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 3 + "," + height / 2 + ")");
     // Create dummy data
     // this is the model data that will be changed to data from the database
 
-
     // set the color scale
-    var color = d3.scaleOrdinal().domain(data).range(['red', 'yellow', 'orange', 'brown', 'pink'])
+    var color = d3
+        .scaleOrdinal()
+        .domain(data)
+        .range(["red", "yellow", "orange", "brown", "pink"]);
 
-    document.getElementById('selectCountyButton').addEventListener('change', updatePie)
+    document
+        .getElementById("selectCountyButton")
+        .addEventListener("change", updatePie);
 
     function updatePie() {
-        var parent = d3.select('#my_dataviz')
-        parent.selectAll('svg').remove()
+        var parent = d3.select("#my_dataviz");
+        parent.selectAll("svg").remove();
 
         var svg = d3
-            .select('#my_dataviz')
-            .append('svg')
-            .attr('id', 'svg')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-            .attr('transform', 'translate(' + width / 3 + ',' + height / 2 + ')')
-        var county = document.getElementById('selectCountyButton').value.toUpperCase()
-        var theUrl = "http://localhost/api/masina/count_cars.php?county="
-        theUrl = theUrl.concat(county)
-        console.log(theUrl);
+            .select("#my_dataviz")
+            .append("svg")
+            .attr("id", "svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr(
+                "transform",
+                "translate(" + width / 3 + "," + height / 2 + ")"
+            );
+        var county = document
+            .getElementById("selectCountyButton")
+            .value.toUpperCase();
+        var theUrl = "http://localhost/api/masina/count_cars.php?county=";
+        theUrl = theUrl.concat(county);
         var req = new XMLHttpRequest();
         req.overrideMimeType("application/json");
         req.open("GET", theUrl, true);
         req.onload = function () {
-            var jsonResponse = JSON.parse(req.responseText);
-            buildPie(svg, jsonResponse.records);
+            responseData = JSON.parse(req.responseText);
+            buildPie(svg, responseData.records);
         };
         req.send();
     }
@@ -69,102 +81,130 @@ function buildAll(data) {
     function buildPie(svg, data) {
         // Compute the position of each group on the pie:
         var pie = d3.pie().value(function (d) {
-            return d.number_of_cars
-        })
-        var data_ready = pie(data)
+            return d.number_of_cars;
+        });
+        var data_ready = pie(data);
         console.log(data);
-        svg.selectAll('whatever')
+        svg.selectAll("whatever")
             .data(data_ready)
             .enter()
-            .append('path')
+            .append("path")
             .transition()
             .delay(function (d, i) {
                 return i * 100;
             })
             .duration(1000)
-            .attr('d', d3.arc().innerRadius(0).outerRadius(radius))
-            .attr('fill', function (d) {
-                return color(d.data.year)
+            .attr("d", d3.arc().innerRadius(0).outerRadius(radius))
+            .attr("fill", function (d) {
+                return color(d.data.year);
             })
-            .attr('stroke', 'black')
-            .style('stroke-width', '1px')
-            .style('opacity', 0.7)
+            .attr("stroke", "black")
+            .style("stroke-width", "1px")
+            .style("opacity", 0.7);
 
-        var size = 20
-        svg.selectAll('mydots')
+        var size = 20;
+        svg.selectAll("mydots")
             .data(data)
             .enter()
-            .append('rect')
-            .attr('x', 220)
+            .append("rect")
+            .attr("x", 220)
             .transition()
             .duration(1000)
-            .attr('y', function (d, i) {
-                return 100 + i * (size + 5)
+            .attr("y", function (d, i) {
+                return 100 + i * (size + 5);
             }) // 100 is where the first dot appears. 25 is the distance between dots
-            .attr('width', size)
-            .attr('height', size)
-            .style('fill', function (d) {
-                return color(d.year)
-            })
+            .attr("width", size)
+            .attr("height", size)
+            .style("fill", function (d) {
+                return color(d.year);
+            });
 
         // Add one dot in the legend for each name.
-        svg.selectAll('mylabels')
+        svg.selectAll("mylabels")
             .data(data)
             .enter()
-            .append('text')
-            .attr('x', 220 + size * 1.2)
+            .append("text")
+            .attr("x", 220 + size * 1.2)
             .transition()
             .duration(1000)
-            .attr('y', function (d, i) {
-                return 100 + i * (size + 5) + size / 2
+            .attr("y", function (d, i) {
+                return 100 + i * (size + 5) + size / 2;
             }) // 100 is where the first dot appears. 25 is the distance between dots
             .text(function (d) {
-                return d.year
+                return d.year;
             })
-            .attr('text-anchor', 'left')
-            .style('alignment-baseline', 'middle')
-
+            .attr("text-anchor", "left")
+            .style("alignment-baseline", "middle");
     }
 
-    buildPie(svg, data)
+    buildPie(svg, data);
+}
 
-    document.getElementById('downloadSVGButton').addEventListener('click', () => saveSvg(document.getElementById('svg'), 'image.svg'))
+document
+    .getElementById("downloadSVGButton")
+    .addEventListener("click", () =>
+        saveSvg(document.getElementById("svg"), "image.svg")
+    );
 
-    function saveSvg(svgEl, name) {
-        svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        var svgData = svgEl.outerHTML;
-        var preface = '<?xml version="1.0" standalone="no"?>\r\n';
-        var svgBlob = new Blob([preface, svgData], { type: "image/svg+xml;charset=utf-8" });
-        var svgUrl = URL.createObjectURL(svgBlob);
-        var downloadLink = document.createElement("a");
-        downloadLink.href = svgUrl;
-        downloadLink.download = name;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-    }
+document
+    .getElementById("csvDownloadButton")
+    .addEventListener("click", () => saveCsv(responseData));
 
-    document.getElementById('webpDownloadButton').addEventListener('click', () => saveWebp())
+function saveSvg(svgEl, name) {
+    svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    var svgData = svgEl.outerHTML;
+    var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+    var svgBlob = new Blob([preface, svgData], {
+        type: "image/svg+xml;charset=utf-8",
+    });
+    var svgUrl = URL.createObjectURL(svgBlob);
+    var downloadLink = document.createElement("a");
+    downloadLink.href = svgUrl;
+    downloadLink.download = name;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
 
-    function saveWebp() {
-        // the canvg call that takes the svg xml and converts it to a canvas
-        var svgData = document.getElementById("svg").outerHTML;
-        const canvas = document.createElement('canvas');
-        canvas.setAttribute('id', 'svg-canvas')
-        canvg(canvas, svgData);
+document
+    .getElementById("webpDownloadButton")
+    .addEventListener("click", () => saveWebp());
 
-        // the canvas calls to output a png
-        var img = canvas.toDataURL("image/webp");
-        // do what you want with the base64, write to screen, post to server, etc...
+function saveWebp() {
+    // the canvg call that takes the svg xml and converts it to a canvas
+    var svgData = document.getElementById("svg").outerHTML;
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "svg-canvas");
+    canvg(canvas, svgData);
 
-        var downloadLink = document.createElement("a");
-        downloadLink.href = img;
-        downloadLink.download = name;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-        document.body.removeChild(canvas)
-    }
+    // the canvas calls to output a png
+    var img = canvas.toDataURL("image/webp");
+    // do what you want with the base64, write to screen, post to server, etc...
 
+    var downloadLink = document.createElement("a");
+    downloadLink.href = img;
+    downloadLink.download = name;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    document.body.removeChild(canvas);
+}
 
+function saveCsv(data) {
+    debugger;
+    var csvContent = "data:text/csv;charset=utf-8,";
+
+    csvContent += data.records
+        .map((it) => {
+            return Object.values(it).toString();
+        })
+        .join("\n");
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link); // Required for FF
+
+    link.click(); // This will download the data file named "my_data.csv".
 }

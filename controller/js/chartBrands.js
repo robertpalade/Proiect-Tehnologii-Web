e696327967cae8a7824ef2e0b7d2e28831d58ae3
robertpalade <61385985+robertpalade@users.noreply.github.com>
2020-06-09 02:44:16 +0300

    added photos to documentation

--- controller/js/chartBrands.js
+++
@@ -1,5 +0,0 @@
-var button1 = "selectCountyButton";
-var button2 = "selectBrandButton";
-var url = "../api/controller/count_cars_county_brand.php?county=";
-
-build_county_combo(button1, button2, url, combobox_brands, "&brand=");
